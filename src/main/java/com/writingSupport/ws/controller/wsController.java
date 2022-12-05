82907dfbc597194add4abc28054c323390bc7b7c
dongkeonkim <dongkeonkim0810@gmail.com>
2022-12-05 18:08:30 +0900

    작업내용: 설정 추가 작업일자: 2022. 12. 05

--- src/main/java/com/writingSupport/ws/controller/wsController.java
+++ src/main/java/com/writingSupport/ws/controller/wsController.java
@@ -1,15 +1,28 @@
 package com.writingSupport.ws.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @Controller
 public class wsController {
 
     @GetMapping("/")
     public String main() {
+        HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String ip = req.getHeader("X-FORWARDED-FOR");
+
+        if (ip == null) {
+            ip = req.getRemoteAddr();
+        }
+
+        log.info("New Connect: " + ip);
 
         return "index";
     }
-
 }
